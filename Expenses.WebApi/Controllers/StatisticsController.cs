257a67fafe23eeb3b138b7bfb8c196c4a962e45e
pflofif <bogdanvivchar8@gmail.com>
2022-11-16 15:11:09 +0200

    Add project files.

---
+++ Expenses.WebApi/Controllers/StatisticsController.cs
@@ -0,0 +1,25 @@
+﻿using Expenses.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Expenses.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StatisticsController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetExpenseAmountPerCategory()
+        {
+            return Ok(new[]
+            {
+                new{ name = "ddd"},
+                new{ name = "ff"},
+                new{ name = "fdf"},
+                new{ name = "dhdod"},
+                new{ name = "dhjhjdd"},
+            });
+        }
+    }
+}
